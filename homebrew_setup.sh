8ff6e80c9faa028a7213813e132cd13e2be1aa97
Scott Savage <snsavage@gmail.com>
2022-01-18 13:35:43 -0500

    Fix first run issues with Homebrew script

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -15,8 +15,7 @@ echo -e "
 
 echo "Managing Homebrew."
 
-which -s brew
-if [[ $? != 0 ]]; then
+if [[ $(command -v brew) == "" ]]; then
     echo "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
--- zshrc
+++ zshrc
@@ -3,10 +3,3 @@ eval "$(starship init zsh)"
 
 export FZF_DEFAULT_COMMAND='rg --files'
 
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
-
