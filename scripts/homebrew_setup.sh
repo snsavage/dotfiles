713c4fae90ef1a28335460433e7cbda9c1c9e3d1
Scott Savage <snsavage@gmail.com>
2022-01-18 21:36:08 -0500

    Install Oh My Zsh

--- scripts/homebrew_setup.sh
+++ scripts/homebrew_setup.sh
@@ -13,8 +13,6 @@ echo -e "
 =======================================================================
 "
 
-echo "Managing Homebrew."
-
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
