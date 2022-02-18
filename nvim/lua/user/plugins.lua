9316d34069eade3916138b33abfe4c6ab5eacda9
Scott Savage <snsavage@gmail.com>
2022-02-17 21:09:55 -0500

    Add nvim web devicons

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -42,6 +42,14 @@ return require('packer').startup(function(use)
   -- Let Packer manager itself
   use "wbthomason/packer.nvim"
 
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+  }
+
   -- Misc Plugins
   use "altercation/vim-colors-solarized"
   use "tpope/vim-commentary"
