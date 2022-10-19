43634dc7809abc5074a67b3714e01ea26d6254b7
Scott Savage <snsavage@gmail.com>
2022-10-18 21:13:18 -0400

    Fix cmp nvim deprecation

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -35,14 +35,12 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 vim.cmd [[autocmd BufWritePre *.go lua vim.lsp.buf.formatting()]]
 -- vim.cmd [[autocmd BufWritePre *.go lua goimports(1000)]]
