97ed6513018a83a7e4d0e60a9758cef1f01ccbc5
Scott Savage <snsavage@gmail.com>
2022-12-19 14:56:09 -0500

    Remove gopls

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,7 +1,13 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_ok then
   return
 end
+
+local util_ok, util = pcall(require, "lspconfigi/util")
+if not util_ok then
+  return
+end
+
 
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
