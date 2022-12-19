e4b7e6ec84c610265bca41f21db1ae4d6e4b57c8
Scott Savage <snsavage@gmail.com>
2022-12-19 14:34:01 -0500

    Disable vim-go

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -60,7 +60,7 @@ return require('packer').startup(function(use)
       'nvim-lua/plenary.nvim'
     },
   }
-  use { "fatih/vim-go", run =':GoUpdateBinaries' }
+  -- use { "fatih/vim-go", run =':GoUpdateBinaries' }
 
   -- NVim Plugings
   use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
