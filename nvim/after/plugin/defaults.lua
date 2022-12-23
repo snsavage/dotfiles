397e6a8c5838e5a1ac5be97714292724588e392b
Scott Savage <snsavage@gmail.com>
2022-12-23 10:17:53 -0500

    Migrate nvim options WIP

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -2,13 +2,21 @@
 local opts = { noremap = true, silent = true } -- Keymap options
 local keymap = vim.api.nvim_set_keymap -- Shorten keymap
 
+vim.opt.backup = false                          -- creates a backup file
+vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+vim.opt.colorcolumn = "+1"
 vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
 vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
+vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
 vim.opt.smartindent = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
+vim.opt.swapfile = false                        -- creates a swapfile
+vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+vim.opt.textwidth = 80
 
 -- Keymaps
 keymap("n", "<leader>w", ":w<cr>", opts) -- Write file
