53d3eceb58d7e62212b05ef02cdfbfab3c9e8c52
Scott Savage <snsavage@gmail.com>
2023-11-09 16:35:33 -0500

    Configure helm-ls for nvim

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -12,16 +12,39 @@ return function(use)
   use "folke/trouble.nvim"
 
   use({
-  "princejoogie/dir-telescope.nvim",
-  -- telescope.nvim is a required dependency
-  requires = {"nvim-telescope/telescope.nvim"},
-  config = function()
-    require("dir-telescope").setup({
-      -- these are the default options set
-      hidden = true,
-      no_ignore = false,
-      show_preview = true,
-    })
-  end,
-})
+    "princejoogie/dir-telescope.nvim",
+    -- telescope.nvim is a required dependency
+    requires = {"nvim-telescope/telescope.nvim"},
+    config = function()
+      require("dir-telescope").setup({
+        -- these are the default options set
+        hidden = true,
+        no_ignore = false,
+        show_preview = true,
+      })
+    end,
+  })
+
+  use "towolf/vim-helm"
+
+  local configs = require('lspconfig.configs')
+  local lspconfig = require('lspconfig')
+  local util = require('lspconfig.util')
+
+  if not configs.helm_ls then
+    configs.helm_ls = {
+      default_config = {
+        cmd = {"helm_ls", "serve"},
+        filetypes = {'helm'},
+        root_dir = function(fname)
+          return util.root_pattern('Chart.yaml')(fname)
+        end,
+      },
+    }
+  end
+
+  lspconfig.helm_ls.setup {
+    filetypes = {"helm"},
+    cmd = {"helm_ls", "serve"},
+  }
 end
