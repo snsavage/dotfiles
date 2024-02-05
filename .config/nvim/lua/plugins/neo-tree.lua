9919561c39eb982a47aae8f5bf53fb6cb18add76
Scott Savage <snsavage@gmail.com>
2024-02-05 08:48:19 -0500

    Configure neo-tree

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  setup = {
+    filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
