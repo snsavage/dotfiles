34f0a17262dece1b1329ffe5710e182a785c2f61
Scott Savage <snsavage@gmail.com>
2024-07-11 21:01:40 -0400

    Configure lualine plugin

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,28 @@
+local icons = LazyVim.config.icons
+
 return {
   "nvim-lualine/lualine.nvim",
   options = {
-    theme = "tokyonight",
+    theme = "tokyonight-day",
+  },
+  sections = {
+    lualine_c = {
+      LazyVim.lualine.root_dir(),
+      {
+        "diagnostics",
+        symbols = {
+          error = icons.diagnostics.Error,
+          warn = icons.diagnostics.Warn,
+          info = icons.diagnostics.Info,
+          hint = icons.diagnostics.Hint,
+        },
+      },
+      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+      -- { LazyVim.lualine.pretty_path() },
+      {
+        "filename",
+        path = 0,
+      },
+    },
   },
 }
