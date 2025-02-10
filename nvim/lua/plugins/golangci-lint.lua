e72f0a70422a5b7211d5799f9ce7f1ad6b3ffccd
Scott Savage <snsavage@gmail.com>
2025-02-10 10:19:46 -0500

    Move and update nvim

---
+++ nvim/lua/plugins/golangci-lint.lua
@@ -0,0 +1,32 @@
+-- local lspconfig = require("lspconfig")
+--
+-- lspconfig.golangci_lint_ls.setup({
+--   filetypes = { "go", "gomod" },
+-- })
+--
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       golangcilsp = {
+--         default_config = {
+--           cmd = { "golangci-lint-langserver" },
+--           root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+--           init_options = {
+--             command = {
+--               "golangci-lint",
+--               "run",
+--               -- "--enable-all",
+--               -- "--disable",
+--               -- "lll",
+--               "--out-format",
+--               "json",
+--               "--issues-exit-code=1",
+--             },
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
+return {}
