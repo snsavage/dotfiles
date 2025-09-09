d15b0b4c7be1edd00fd352c948f2d9829503cc3c
Scott Savage <snsavage@gmail.com>
2025-09-09 10:22:22 -0400

    Show more path in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,25 @@
-local icons = LazyVim.config.icons
-
 return {
   "nvim-lualine/lualine.nvim",
-  options = {
-    theme = "tokyonight-day",
-  },
-  sections = {
-    lualine_c = {
+  opts = function(_, opts)
+    local icons = LazyVim.config.icons
+
+    opts.options = opts.options or {}
+    opts.options.theme = "tokyonight"
+
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_b = {
+      {
+        "branch",
+        fmt = function(str)
+          if #str > 10 then
+            return str:sub(1, 10) .. "..."
+          end
+          return str
+        end,
+      },
+      "diff",
+    }
+    opts.sections.lualine_c = {
       LazyVim.lualine.root_dir(),
       {
         "diagnostics",
@@ -18,11 +31,9 @@ return {
         },
       },
       { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-      -- { LazyVim.lualine.pretty_path() },
-      {
-        "filename",
-        path = 0,
-      },
-    },
-  },
+      { LazyVim.lualine.pretty_path() },
+    }
+
+    return opts
+  end,
 }
